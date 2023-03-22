fd8ce2b5d94ca6efbf072b165fd53d885d21eaa0
Pedro Henrique <phmartins.developer@gmail.com>
2023-03-22 18:55:14 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+  },
+};
