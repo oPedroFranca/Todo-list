4d0a42c8fdae500d66a529c6c7aec8a24f873cdd
Pedro Henrique <phmartins.developer@gmail.com>
2023-04-27 18:26:32 -0300

    creating welcome Modal

--- src/components/WelcomeModal/ModalHeader/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ModalHeaderDiv } from './style';
-
-export const ModalHeader = () => {
-  return <ModalHeaderDiv></ModalHeaderDiv>;
-};
--- src/components/WelcomeModal/index.tsx
+++ src/components/WelcomeModal/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import { ModalContent, ModalDiv } from './style';
-import { ModalHeader } from './ModalHeader';
 import { ModalMain } from './ModalMain';
 
 export const WelcomeModal = () => {
   return (
     <ModalDiv>
       <ModalContent>
-        <ModalHeader />
-
         <ModalMain />
       </ModalContent>
     </ModalDiv>
