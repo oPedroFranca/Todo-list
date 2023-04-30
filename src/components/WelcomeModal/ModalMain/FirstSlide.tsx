9cef073250f0640147e3ea21c3ff8223e51e0923
Pedro Henrique <phmartins.developer@gmail.com>
2023-04-30 17:18:36 -0300

    creating welcome Modal Slider

--- src/components/WelcomeModal/ModalMain/FirstSlide.tsx
+++ src/components/WelcomeModal/ModalMain/FirstSlide.tsx
@@ -9,6 +9,10 @@ export const FirstSlide = () => {
           <h1>Welcome</h1> to my project
           <h1>!</h1>
         </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit Consectetur
+          sed eveniet veniam rem, alias nesciunt magnam doloribus, facere
+        </p>
       </div>
     </>
   );
