c60e6f7d10b960ef72cb47954648ec58b662bbbe
Pedro Henrique <phmartins.developer@gmail.com>
2023-05-01 21:00:54 -0300

    creating welcome Modal Slider

--- src/components/WelcomeModal/ModalMain/Slider/Slider.tsx
+++ src/components/WelcomeModal/ModalMain/Slider/Slider.tsx
@@ -1,38 +1,38 @@
-/* eslint-disable react/jsx-key */
-import React, { useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
+// /* eslint-disable react/jsx-key */
+// import React, { useState } from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Navigation } from 'swiper';
 
-import './style.css';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { FirstSlide } from './FirstSlide';
-import { SecondSlide } from './SecondSlide';
-import { ThirdSlide } from './ThirdSlide';
+// import './style.css';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import { FirstSlide } from './FirstSlide';
+// import { SecondSlide } from './SecondSlide';
+// import { ThirdSlide } from './ThirdSlide';
 
-export const Slider = () => {
-  const [swiper, setSwiper] = useState(null);
-  const slides = [<FirstSlide />, <SecondSlide />, <ThirdSlide />];
+// export const Slider = () => {
+//   const [swiper, setSwiper] = useState(null);
+//   const slides = [<FirstSlide />, <SecondSlide />, <ThirdSlide />];
 
-  const handleNextClick = () => {
-    if (swiper) {
-      swiper.slideNext();
-    }
-  };
-  return (
-    <Swiper
-      modules={[Navigation]}
-      style={{ marginLeft: '0px' }}
-      navigation
-      spaceBetween={50}
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => setSwiper(swiper)}
-    >
-      {slides.map((slide, index) => (
-        <SwiperSlide key={index}>{slide}</SwiperSlide>
-      ))}
-      <button onClick={handleNextClick}>next</button>
-    </Swiper>
-  );
-};
+//   const handleNextClick = () => {
+//     if (swiper) {
+//       swiper.slideNext();
+//     }
+//   };
+//   return (
+//     <Swiper
+//       modules={[Navigation]}
+//       style={{ marginLeft: '0px' }}
+//       navigation
+//       spaceBetween={50}
+//       slidesPerView={1}
+//       onSlideChange={() => console.log('slide change')}
+//       onSwiper={(swiper) => setSwiper(swiper)}
+//     >
+//       {slides.map((slide, index) => (
+//         <SwiperSlide key={index}>{slide}</SwiperSlide>
+//       ))}
+//       <button onClick={handleNextClick}>next</button>
+//     </Swiper>
+//   );
+// };
--- src/components/WelcomeModal/ModalMain/index.tsx
+++ src/components/WelcomeModal/ModalMain/index.tsx
@@ -1,18 +1,62 @@
-import React from 'react';
+/* eslint-disable react/display-name */
+import React, { useImperativeHandle, useRef, useState } from 'react';
 import { SubmitButtonModal } from '../SubmitButton';
 import { ModalMainDiv } from './style';
 
-import { Slider } from './Slider/Slider';
+/* eslint-disable react/jsx-key */
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { FirstSlide } from './Slider/FirstSlide';
+import { SecondSlide } from './Slider/SecondSlide';
+import { ThirdSlide } from './Slider/ThirdSlide';
+import { Navigation } from 'swiper';
+
+const Slider = React.forwardRef((_props, ref) => {
+  const [swiper, setSwiper] = useState(null);
+  const slides = [<FirstSlide />, <SecondSlide />, <ThirdSlide />];
+
+  const handleNextClick = () => {
+    if (swiper) {
+      swiper.slideNext();
+    }
+  };
+
+  useImperativeHandle(ref, () => ({
+    handleNextClick,
+  }));
+
+  return (
+    <Swiper
+      modules={[Navigation]}
+      style={{ marginLeft: '0px' }}
+      navigation
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => setSwiper(swiper)}
+    >
+      {slides.map((slide, index) => (
+        <SwiperSlide key={index}>{slide}</SwiperSlide>
+      ))}
+    </Swiper>
+  );
+});
 
 export const ModalMain = () => {
+  const sliderRef = useRef(null);
+
   const handleNextClick = () => {
-    console.log('cloicado');
+    if (sliderRef.current) {
+      sliderRef.current.handleNextClick();
+    }
   };
 
   return (
     <>
       <ModalMainDiv>
-        <Slider />
+        <Slider ref={sliderRef} />
       </ModalMainDiv>
       <SubmitButtonModal onClick={handleNextClick} />
     </>
