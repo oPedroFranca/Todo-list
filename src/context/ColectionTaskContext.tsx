9bf4a260ccc560c7958bd61b41f6cc7bb46232a4
Pedro Henrique <phmartins.developer@gmail.com>
2023-06-02 18:01:51 -0300

    saving tasks in localStorage

--- src/context/ColectionTaskContext.tsx
+++ src/context/ColectionTaskContext.tsx
@@ -44,6 +44,20 @@ export const CollectionTaskProvider: React.FC<{
     localStorage.setItem('tasks', JSON.stringify(tasks));
   };
 
+  const addTask = ({ taskName, taskDescription }: Task) => {
+    const taskId = uuidv4();
+    const newTask: Task = {
+      taskId,
+      taskName,
+      taskDescription,
+    };
+
+    const updatedTasks = [...taskList, newTask];
+    setTaskList(updatedTasks);
+
+    saveTasksToLocalStorage(updatedTasks);
+  };
+
   const removeTask = (taskId: string) => {
     const updatedTasks = taskList.filter((task) => task.taskId !== taskId);
     setTaskList(updatedTasks);
