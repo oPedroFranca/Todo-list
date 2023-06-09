926f45348f4fde0c1bb2f2944de9b50fa0d0dc10
Pedro Henrique <phmartins.developer@gmail.com>
2023-06-08 21:14:05 -0300

    completing current day of created task

--- src/components/Task/Date/index.tsx
+++ src/components/Task/Date/index.tsx
@@ -5,3 +5,14 @@ import { DateDiv } from './style';
 type DateTaskProps = {
   value: string;
 };
+
+export const DateToday = ({ value }: DateTaskProps) => {
+  return (
+    <DateDiv>
+      <RiCalendarCheckFill
+        style={{ marginRight: '3px', width: '13px', height: '13px' }}
+      />
+      {value}
+    </DateDiv>
+  );
+};
