8a26e6eefa8f985f98cc36f1de968efdc6315e32
Pedro Henrique <phmartins.developer@gmail.com>
2023-06-21 14:45:57 -0300

    Creating ActivityTasks

--- src/components/TaskDetails/AddSubTask/index.tsx
+++ src/components/TaskDetails/AddSubTask/index.tsx
@@ -4,7 +4,7 @@ import { AddButton, AddTaskContent } from './style';
 import { TaskSelected } from '../../../utils/TaskSelectedDetails';
 import { DetailTaskContext } from '../../../context/DetailsTasks';
 
-export const AddTask = ({
+export const AddSubTask = ({
   onAddSubtask,
 }: {
   onAddSubtask: (newSubtask: any) => void;
@@ -14,6 +14,27 @@ export const AddTask = ({
 
   const handleAddDetailsTask = () => {
     const newSubtask = addDetailTaskToTask(taskSelected!);
+
+    // Check if taskSelected is defined
+    if (taskSelected) {
+      // Get tasks from Local Storage
+      const savedTasks = localStorage.getItem('tasks');
+      const tasks = savedTasks ? JSON.parse(savedTasks) : [];
+
+      // Find the task with the corresponding ID in Local Storage
+      const selectedTask = tasks.find(
+        (task: { taskId: string }) => task.taskId === taskSelected.taskId,
+      );
+
+      if (selectedTask) {
+        // Add the new subtask to the array of subtasks in the task
+        selectedTask.subtasks.push(newSubtask);
+
+        // Save the updated tasks back to Local Storage
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      }
+    }
+
     onAddSubtask(newSubtask);
   };
 
--- src/components/TaskDetails/Main/index.tsx
+++ src/components/TaskDetails/Main/index.tsx
@@ -1,26 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { AllTasks, NumberTasks, TaskDetailsMain } from './style';
 import { ActivityTasks } from '../ActivityTasks';
-import { AddTask } from '../AddTask';
+import { AddSubTask } from '../AddSubTask';
 import { TaskSelected } from '../../../utils/TaskSelectedDetails';
 import { Subtask } from '../../../context/DetailsTasks';
 
 export const MainTaskDetails = () => {
   const taskSelected = TaskSelected();
-  const [subtasks, setSubtasks] = useState<Subtask[]>(taskSelected!.subtasks);
+  const [subtasks, setSubtasks] = useState<Subtask[]>([]);
+
+  useEffect(() => {
+    // Get tasks from Local Storage
+    const savedTasks = localStorage.getItem('tasks');
+    const tasks = savedTasks ? JSON.parse(savedTasks) : [];
+
+    // Find the task with the corresponding ID in Local Storage
+    const selectedTask = tasks.find(
+      (task: { taskId: string }) => task.taskId === taskSelected?.taskId,
+    );
+
+    // Update the subtasks state
+    setSubtasks(selectedTask?.subtasks || []);
+  }, [taskSelected]);
 
   const handleAddSubtask = (newSubtask: Subtask) => {
     const updatedSubtasks = [...subtasks, newSubtask];
     setSubtasks(updatedSubtasks);
-  };
 
-  useEffect(() => {
-    console.log('Subtasks:', subtasks);
-  }, [subtasks]);
+    // Update the subtasks array in the selected task in Local Storage
+    const savedTasks = localStorage.getItem('tasks');
+    const tasks = savedTasks ? JSON.parse(savedTasks) : [];
+    const selectedTaskIndex = tasks.findIndex(
+      (task: { taskId: string }) => task.taskId === taskSelected?.taskId,
+    );
+
+    if (selectedTaskIndex !== -1) {
+      tasks[selectedTaskIndex].subtasks = updatedSubtasks;
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  };
 
   return (
     <TaskDetailsMain>
-      <AddTask onAddSubtask={handleAddSubtask} />
+      <AddSubTask onAddSubtask={handleAddSubtask} />
 
       <NumberTasks>Tasks - {subtasks.length}</NumberTasks>
 
